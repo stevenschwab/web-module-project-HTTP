3c6acf976c62c8d70b5bd5e4cf6fde0fddf882b2
Steve Schwab <schwabs@umich.edu>
2024-04-14 14:44:24 -0700

    adjust how favoriteMovies is used in the FavoriteMovieList component since it will now be working with key-value pairs

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const FavoriteMovieList = ({ favoriteMovies }) => {
-  return (<div className="col-xs savedContainer">
-    <h5>Favorite Movies</h5>
-    {
-      favoriteMovies.map(movie => {
-        return <Link key={movie.id} className="btn btn-light savedButton" to={`/movies/${movie.id}`}>{movie.title}</Link>
-      })
-    }
-  </div>);
-}
+  return (
+    <div className="col-xs savedContainer">
+      <h5>Favorite Movies</h5>
+      {Array.from(favoriteMovies, ([id, title]) => (
+        <Link key={id} className='btn btn-light savedButton' to={`/movies/${id}`}>{title}</Link>
+      ))}
+    </div>
+  );
+};
 
 export default FavoriteMovieList;
